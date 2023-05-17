a4b1fa94d44cfdb8d79eeb9062bd14c4f3b7fef6
HenriqueSchneiderAlmeida <125312791+HenriqueSchneiderAlmeida@users.noreply.github.com>
2023-05-17 12:35:08 -0300

    inclusão de arquivo js

---
+++ main.js
@@ -0,0 +1,6 @@
+alert ("Bem vindo ao Site das Batatas.\nPeça 3 Batatas e ganhe uns quilos a mais!");
+
+function funcao1()
+{
+alert("Pergunte assim: \nPor favor, você tem uma previsão de tempo!");
+}
